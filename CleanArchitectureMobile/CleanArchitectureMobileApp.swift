ae8bb33dac79e8c4d807d36d3f2f39291a3564e2
Matheus Lima Gomes <matheus.lima.gomes@outlook.com>
2022-10-17 22:59:37 -0300

    Initial Commit

---
+++ CleanArchitectureMobile/CleanArchitectureMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureMobileApp.swift
+//  CleanArchitectureMobile
+//
+//  Created by Matheus F S L Gomes on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
