11965a357caab361f87e19312b32d5fae3638345
Matheus Lima Gomes <matheus.lima.gomes@outlook.com>
2022-11-17 19:35:43 -0300

    add Project Domain & Data frameworks

---
+++ Domain/Project.swift
@@ -0,0 +1,32 @@
+import ProjectDescription
+
+
+let infoPlist: [String: InfoPlist.Value] = [
+    "CFBundleDevelopmentRegion": "$(DEVELOPMENT_LANGUAGE)",
+    "CFBundleExecutable": "$(EXECUTABLE_NAME)",
+    "CFBundleIdentifier": "$(PRODUCT_BUNDLE_IDENTIFIER)",
+    "CFBundleInfoDictionaryVersion": "6.0",
+    "CFBundleName": "$(PRODUCT_NAME)",
+    "CFBundlePackageType": "APPL",
+    "CFBundleShortVersionString": "1.0",
+    "CFBundleVersion": "1",
+    "LSRequiresIPhoneOS": true,
+    "NSHumanReadableCopyright": "Copyright ©. All rights reserved.",
+    "Test": "Value",
+]
+
+let organizationName = "br.com.Jaus-Technology"
+let project = Project(
+    name: "Domain",
+    targets: [
+        Target(
+            name: "Domain",
+            platform: .macOS,
+            product: .framework,
+            bundleId: "\(organizationName).domain",
+            infoPlist: .dictionary(infoPlist),
+            sources: ["Source/**"],
+            resources: ["Resource/**"]
+        )
+    ]
+)
--- Project.swift
+++ Project.swift
@@ -3,15 +3,5 @@ import ProjectDescription
 let organizationName = "br.com.Jaus-Technology"
 let project = Project(
     name: "CleanArchitectureMobile",
-    targets: [
-        Target(
-            name: "Domain",
-            platform: .macOS,
-            product: .framework,
-            bundleId: "\(organizationName).domain",
-            infoPlist: "Domain/Resource/",
-            sources: ["Domain/Source/**"],
-            resources: ["Domain/Resource/**"]
-        )
-    ]
+    targets: []
 )
