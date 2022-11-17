54723800b900528c262225c9917f3e313622973f
Matheus Lima Gomes <matheus.lima.gomes@outlook.com>
2022-11-16 22:54:26 -0300

    move class helper to extension class

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -31,12 +31,14 @@ final class RemoteAddAccountTests: XCTestCase {
         XCTAssertEqual(httpClientSpy.url, url)
     }
 
+
+}
+
+extension RemoteAddAccountTests {
     class HttpClientSpy: HttpPostClient {
         var url: URL?
-
         func post(url: URL) {
             self.url = url
         }
     }
-
 }
