436bd69df168d284a1e4ad0aaacd41d6917ba3fc
Matheus Lima Gomes <matheus.lima.gomes@outlook.com>
2022-11-17 16:35:27 -0300

    add config to Domain target

--- Project.swift
+++ Project.swift
@@ -1,8 +1,21 @@
 import ProjectDescription
 
+let organizationName = "br.com.Jaus-Technology"
 let project = Project(
     name: "CleanArchitectureMobile",
-    organizationName: "br.com.Jaus-Technology",
+    organizationName: organizationName,
     settings: nil, 
-    targets: []
+    targets: [
+        Target(
+            name: "Domain",
+            platform: .macOS,
+            product: .framework,
+            bundleId: "\(organizationName).domain",
+            infoPlist: "Domain/Info.plist",
+            sources: ["Domain/Source/**"],
+            resources: ["Domain/Resource/**"],
+            dependencies: [],
+            settings: nil
+        )
+    ]
 )
